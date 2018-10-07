a2432bb34c7cac4a8355c26ce60cef591f844f07
coderadi369 <adithya.iiitm@gmail.com>
2018-10-07 23:52:43 +0530

    completed patch operations

--- router.js
+++ router.js
@@ -1,6 +1,7 @@
 const router = module.exports = require('express').Router()
 const {IsAuthenticated,JWT} = require(`${__base}/utils/user`)
 const cors = require('cors')
+const jsonpatch=require('json-patch')
 
 var corsOptions = {
     credentials: true
@@ -27,10 +28,14 @@ router.post('/login', (req, res) => {
 })
 
 
-router.post('/path', IsAuthenticated, (req, res) => {
-    res.status(200).json({
-        'message': 'router for JSON patch'
-    })
+router.post('/patch', IsAuthenticated, (req, res) => {
+    const {body,patch}=req.body
+    if(!(body) || !(patch)){
+    	res.status(400).json({'message':'Both body and patch are required fields'})
+    }else{
+    	const result=jsonpatch.apply(body,patch)
+    	res.status(200).json(result)
+    }
 })
 
 router.post('/thumbnail', IsAuthenticated, (req, res) => {
